4659d3eb4dd14e0bf4fb04a3baa8355c5943b5d6
DevGenix <you@example.com>
2020-12-06 16:07:49 -0500

    Header component compelte

--- src/Header.js
+++ src/Header.js
@@ -11,21 +11,27 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <MenuIcon />
-      <img
-        className="header__logo"
-        src="https://static.wikia.nocookie.net/logopedia/images/1/19/YouTube_logo_2017.svg/revision/latest/scale-to-width-down/300?cb=20190515171106"
-        alt=""
-      />
-      <input type="text" />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
-      <Avatar
-        alt="Shashank Ellareddy"
-        src="https://avatars0.githubusercontent.com/u/66846202?s=460&u=3ede97a61b4a1690882fb59dceb8bbf9b188c3c7&v=4"
-      />
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://static.wikia.nocookie.net/logopedia/images/1/19/YouTube_logo_2017.svg/revision/latest/scale-to-width-down/300?cb=20190515171106"
+          alt=""
+        />
+      </div>
+      <div className="header__input">
+        <input placeholder="Search" type="text" />
+        <SearchIcon className="header__inputButton" />
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar
+          alt="Shashank Ellareddy"
+          src="https://avatars0.githubusercontent.com/u/66846202?s=460&u=3ede97a61b4a1690882fb59dceb8bbf9b188c3c7&v=4"
+        />
+      </div>
     </div>
   );
 }
