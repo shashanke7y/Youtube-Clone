1bff2a7452455955b801b8fb3421ec912ddd4186
DevGenix <you@example.com>
2020-12-05 21:55:19 -0500

    Removed boiler plate code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build the youtube clone 🔥🚀</h1>
+      <Header />
     </div>
   );
 }
