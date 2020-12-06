3effe69dd0c4d72ea754784c5b9c86f7955669f9
DevGenix <you@example.com>
2020-12-05 21:32:34 -0500

    boiler plate code

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBeTKUAMqvyT1MHwdrb7E4V1T2BIwdcMKo",
+  authDomain: "clone-18.firebaseapp.com",
+  projectId: "clone-18",
+  storageBucket: "clone-18.appspot.com",
+  messagingSenderId: "117054022740",
+  appId: "1:117054022740:web:c966181ecb9ef5d28979c1",
+  measurementId: "G-G39B720F2F",
+};
